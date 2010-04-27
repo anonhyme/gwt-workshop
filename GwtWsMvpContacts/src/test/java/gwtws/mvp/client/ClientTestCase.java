772cf0ccff8462aed27d9e509ca1b13e22f40917
manuel.carrasco.m <manuel.carrasco.m@55d144da-97c4-e010-bb1c-32a49dc6fa98>
2010-04-27 22:16:04 +0000

    increased code coverage

--- GwtWsMvpContacts/src/test/java/gwtws/mvp/client/ClientTestCase.java
+++ GwtWsMvpContacts/src/test/java/gwtws/mvp/client/ClientTestCase.java
@@ -1,26 +1,9 @@
-/****************************************************************
- * Licensed to the Apache Software Foundation (ASF) under one   *
- * or more contributor license agreements.  See the NOTICE file *
- * distributed with this work for additional information        *
- * regarding copyright ownership.  The ASF licenses this file   *
- * to you under the Apache License, Version 2.0 (the            *
- * "License"); you may not use this file except in compliance   *
- * with the License.  You may obtain a copy of the License at   *
- *                                                              *
- *   http://www.apache.org/licenses/LICENSE-2.0                 *
- *                                                              *
- * Unless required by applicable law or agreed to in writing,   *
- * software distributed under the License is distributed on an  *
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
- * KIND, either express or implied.  See the License for the    *
- * specific language governing permissions and limitations      *
- * under the License.                                           *
- ****************************************************************/
 package gwtws.mvp.client;
 
 import gwtws.mvp.client.guice.ClientTestModule;
 import gwtws.mvp.server.guice.ServerModule;
 import junit.framework.TestCase;
+import net.customware.gwt.dispatch.client.DispatchAsync;
 import net.customware.gwt.presenter.client.EventBus;
 
 import com.google.gwt.junit.GWTMockUtilities;
@@ -39,4 +22,5 @@ public abstract class ClientTestCase extends TestCase {
 
 	protected Injector injector = Guice.createInjector(new ServerModule(), new ClientTestModule());
 	protected EventBus eventBus = injector.getInstance(EventBus.class);
+	protected DispatchAsync dispatcher = injector.getInstance(DispatchAsync.class);
 }
