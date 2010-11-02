dfdf785f280c80852643415b1b5414fcd9f97692
manuel.carrasco.m <manuel.carrasco.m@55d144da-97c4-e010-bb1c-32a49dc6fa98>
2010-11-02 00:08:17 +0000

    Exercise: porting code from jquery tutorial at http://docs.jquery.com/Tutorials:Live_Examples_of_jQuery

--- GwtWsGQuerySample/src/main/java/com/decharlas/gquerysample/client/GQuerySample.java
+++ GwtWsGQuerySample/src/main/java/com/decharlas/gquerysample/client/GQuerySample.java
@@ -1,29 +1,95 @@
 package com.decharlas.gquerysample.client;
-import com.google.gwt.dom.client.Element;
-import com.google.gwt.query.client.Function;
+
 import static com.google.gwt.query.client.GQuery.*;
 import static com.google.gwt.query.client.plugins.Effects.Effects;
 
-
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.query.client.Function;
+import com.google.gwt.query.client.plugins.Effects.Speed;
+import com.google.gwt.user.client.Window;
 
 /**
  * Example code for a GwtQuery application
  */
 public class GQuerySample implements EntryPoint {
 
-  public void onModuleLoad() {
-    
-    $("div")
-    .hover(new Function() {
-      public void f(Element e) {
-        $(e).css("color", "blue").as(Effects).stop().animate("fontSize: '+=10px'");
-      }
-    }, new Function() {
-      public void f(Element e) {
-        $(e).css("color", "").as(Effects).stop().animate("fontSize: '-=10px'");
-      }
-    });
-  }
+	public void onModuleLoad() {
+
+		$("#hello").hover(new Function() {
+			public void f(Element e) {
+				$(e).css("color", "blue").as(Effects).stop().animate(
+				    "fontSize: '+=10px'");
+			}
+		}, new Function() {
+			public void f(Element e) {
+				$(e).css("color", "").as(Effects).stop().animate("fontSize: '-=10px'");
+			}
+		});
+		
+		// hide all code example
+		$("pre").hide();
+		// configure links to show/hide code
+		$("a").click(new Function() {
+			public void f(Element e) {
+				String letter = e.getClassName().substring(e.getClassName().length()-1);
+				$("pre.code" + letter).toggle();
+			}
+		});
+
+		// Code for example A
+		$("input.buttonAsize").click(new Function() {
+			public void f(Element e) {
+				Window.alert("" + $("div.contentToChange").find("p").size());
+			}
+		});
+		// Code for example B
+		$("input.buttonBslidedown").click(new Function() {
+			public void f(Element e) {
+				$("div.contentToChange").find("p.firstparagraph").as(Effects).clipDown(
+				    Speed.SLOW);
+			}
+		});
+		$("input.buttonBslideup").click(new Function() {
+			public void f(Element e) {
+				$("div.contentToChange").find("p.firstparagraph").as(Effects).clipUp(
+				    Speed.SLOW);
+			}
+		});
+		// Code for example C
+		$("input.buttonCAdd").click(new Function() {
+			public void f(Element e) {
+				$("div.contentToChange p:not(.alert)").append("<strong class='addedtext'> This text was just appended to this paragraph</strong>");
+			}
+		});
+		$("input.buttonCRemove").click(new Function() {
+			public void f(Element e) {
+				$("strong.addedtext").remove();
+			}
+		});
+		// Code for example D
+		$("input.buttonDhide").click(new Function() {
+			public void f(Element e) {
+				$("div.contentToChange p.thirdparagraph").as(Effects).fadeOut(Speed.SLOW);
+			}
+		});
+		// Code for example E
+		$("input.buttonEitalics").click(new Function() {
+			public void f(Element e) {
+				$("div.contentToChange em").css($$("{color:'#993300', fontWeight:'bold'}"));
+			}
+		});
+		// Code for example F
+		$("input.buttonFaddclass").click(new Function() {
+			public void f(Element e) {
+				$("p.fifthparagraph").addClass("changeP");
+			}
+		});
+		$("input.buttonFremoveclass").click(new Function() {
+			public void f(Element e) {
+				$("p.fifthparagraph").removeClass("changeP");
+			}
+		});
+	}
 
 }
